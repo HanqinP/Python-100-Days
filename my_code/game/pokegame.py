4181f439d49dc86b072c9cc69f61b7573b486f36
hanqin <phq1027@gmail.com>
2025-12-11 01:50:33 +0800

    251211-pokegame.py

--- my_code/game/pokegame.py
+++ my_code/game/pokegame.py
@@ -37,3 +37,37 @@ class Poker:
         card = self.cards[self.current]
         self.current += 1
         return card
+    
+    @property
+    def has_next(self):
+        """是否还有牌"""
+        return self.current < len(self.cards)
+    
+class Player:
+    """玩家"""
+
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+    
+    def receive(self, card):
+        """接收牌"""
+        self.cards.append(card)
+    
+    def arrange(self):
+        """整理牌"""
+        self.cards.sort(key=lambda card: (card.suite.value, card.face))
+
+    
+poker = Poker()
+poker.shuffle()
+players = [Player('东邪'), Player('西毒'), Player('南帝'), Player('北丐')]
+# 将牌轮流发到每个玩家手上每人13张牌
+for _ in range(13):
+    for player in players:
+        player.receive(poker.deal())
+# 玩家整理手上的牌输出名字和手牌
+for player in players:
+    player.arrange()
+    print(f'{player.name}: ', end='')
+    print(player.cards)
