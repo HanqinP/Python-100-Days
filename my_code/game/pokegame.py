a57fb57a10a6f7818476c181dc11aa27870d69be
hank <hanqinp@gmail.com>
2025-12-10 15:54:15 +0000

    251210-pokegame.py

---
+++ my_code/game/pokegame.py
@@ -0,0 +1,39 @@
+from enum import Enum
+import random
+
+class Suite(Enum):
+    """花色（枚举）"""
+    SPADE, HEART, CLUB, DIAMOND = range(4)
+
+class Card:
+    """牌"""
+    
+    def __init__(self, suite, face):
+        self.suite = suite
+        self.face = face
+
+    def __repr__(self):
+        suites = '♠♥♣♦'
+        faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8',
+        '9', '10', 'J', 'Q', 'K']
+        return f'{suites[self.suite.value]}{faces[self.face]}'
+
+
+class Poker:
+    """扑克"""
+
+    def __init__(self):
+        self.cards = [Card(suite, face) for suite in Suite 
+                                        for face in range(1, 14)]
+        self.current = 0
+
+    def shuffle(self):
+        """洗牌"""
+        self.current = 0
+        random.shuffle(self.cards)
+
+    def deal(self):
+        """发牌"""
+        card = self.cards[self.current]
+        self.current += 1
+        return card
